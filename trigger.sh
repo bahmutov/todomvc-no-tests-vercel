bb4ba50527d3798e0577927037e19839eea52c01
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-07-27 11:37:54 -0400

    use proper OR

--- trigger.sh
+++ trigger.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 # to run inject the environment variable using as-a utility
 # as-a circleci-user ./trigger.sh
@@ -11,7 +11,7 @@ curl -u ${CIRCLE_CI_API_TOKEN}: \
   --data branch=rename-attribute2 \
   --data parameters[TEST_BRANCH]=rename-attribute \
   --data parameters[TEST_URL]=${TEST_URL} \
-  ${TEST_PIPELINE_URL} | \
+  ${TEST_PIPELINE_URL} || \
 curl -u ${CIRCLE_CI_API_TOKEN}: \
   --silent \
   --data branch=rename-attribute \
