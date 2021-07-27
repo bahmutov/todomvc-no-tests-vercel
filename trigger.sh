43b37bd211874cccf399ba3234ec98c209862d6a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-07-27 12:08:35 -0400

    rename the remove attribute (#7)
    * rename the remove attribute
    
    * trigger the branch with the same name
    
    * trigger build on branch rename-attribute
    
    * [skip ci] add trigger script
    
    * trigger build on branch rename-attribute again
    
    * trigger the branch
    
    * add fallback trigger
    
    * check pipeline
    
    * use proper OR
    
    * back to remove data-cy attribute

--- trigger.sh
+++ trigger.sh
@@ -1,10 +1,20 @@
-#! /bin/sh
+#! /bin/bash
 
 # to run inject the environment variable using as-a utility
 # as-a circleci-user ./trigger.sh
+
+export TEST_URL=https://todomvc-no-tests-vercel-86m2n6fxp-gleb-bahmutov.vercel.app
+
+# if the first branch does not exist, try the alternative
+curl -u ${CIRCLE_CI_API_TOKEN}: \
+  --silent \
+  --data branch=rename-attribute2 \
+  --data parameters[TEST_BRANCH]=rename-attribute \
+  --data parameters[TEST_URL]=${TEST_URL} \
+  ${TEST_PIPELINE_URL} || \
 curl -u ${CIRCLE_CI_API_TOKEN}: \
   --silent \
   --data branch=rename-attribute \
   --data parameters[TEST_BRANCH]=rename-attribute \
-  --data parameters[TEST_URL]=https://todomvc-no-tests-vercel-86m2n6fxp-gleb-bahmutov.vercel.app \
-  https://circleci.com/api/v2/project/gh/bahmutov/todomvc-tests-circleci/pipeline
+  --data parameters[TEST_URL]=${TEST_URL} \
+  ${TEST_PIPELINE_URL}
