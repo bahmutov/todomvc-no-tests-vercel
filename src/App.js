125c6c4b612f3508f8ea618484503582ed854e28
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-07-29 10:57:29 -0400

    add completed class

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css'
 export function Todo({ todo, index, toggleTodo, removeTodo }) {
   const toggleText = todo.isCompleted ? 'Redo' : 'Complete'
   const cyAttribute = todo.isCompleted ? 'redo' : 'complete'
+  const className = todo.isCompleted ? 'todo completed' : 'todo'
 
   return (
     <div
-      className="todo"
+      className={className}
       style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
     >
       {todo.text}
